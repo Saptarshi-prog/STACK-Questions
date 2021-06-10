1fb60dccd452b7bd457c23dfa5cd8d2c42cb2a0f
Saptarshi Neogi <neogisaptarshi@gmail.com>
2021-06-10 23:06:44 +0530

    Update Nearest Greater to Right.py

--- Nearest Greater to Right.py
+++ Nearest Greater to Right.py
@@ -2,14 +2,16 @@
 """
 Created on Thu Jun 10 22:48:31 2021
 
-@author: 91842
+@author: Saptarshi
+
+In a given array find the nearest greater element of every element in the array to the right of it. If none is greater return -1.
 """
 arr = list(map(int,input().split()))
 n = len(arr)
 stack = []
 ans = []
 
-for i in range(n-1,-1,-1):      #traversing in revresed manner
+for i in range(n-1,-1,-1):      #traversing in revresed manner i.e traversing from right towards left
     if len(stack)==0:
         ans.append(-1)
     elif len(stack)>0 and stack[-1]>arr[i]:
